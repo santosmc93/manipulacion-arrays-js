f54cb9ed7e008cff0ece7c056465819f33808991
santosmc93 <carlos.santosm93@gmail.com>
2022-01-19 17:30:05 -0600

    Se agrega el archivo de javascript donde se ve el tema de flatmap y sus usos

---
+++ server/15-flatmap.js
@@ -0,0 +1,45 @@
+const users = [
+    { userId: 1, username: "Tom", attributes: ["Nice", "Cute"]},
+    { userId: 2, username: "Mike", attributes: ["Lovely"]},
+    { userId: 3, username: "Nico", attributes: ["Nice", "Cool"]},
+  ]
+
+  const rta = users.map(item => item.attributes).flat();
+  console.log(rta);
+  const rta2 = users.flatMap(item => item.attributes);
+  console.log(rta2);
+
+  const calendars = {
+    primaryCalendar: [
+      {
+        startDate: new Date(2021, 1, 1, 15),
+        endDate: new Date(2021, 1, 1, 15, 30),
+        title: "Cita 1",
+      },
+      {
+        startDate: new Date(2021, 1, 1, 17),
+        endDate: new Date(2021, 1, 1, 18),
+        title: "Cita 2",
+      },
+    ],
+    secondaryCalendar: [
+      {
+        startDate: new Date(2021, 1, 1, 12),
+        endDate: new Date(2021, 1, 1, 12, 30),
+        title: "Cita 2",
+      },
+      {
+        startDate: new Date(2021, 1, 1, 9),
+        endDate: new Date(2021, 1, 1, 10),
+        title: "Cita 4",
+      },
+    ],
+  };
+
+  const rta3 = Object.values(calendars);
+  console.log(rta3);
+  const rta4 = rta3.flatMap(index => {
+      console.log("iteracion" + index);
+      return index.map(item => item.startDate);
+  });
+  console.log(rta4)
